68c2a1a67370374aa1cbea3ade02167382e45a97
Lucious-LT <luciousudemezue@gmail.com>
2023-10-11 13:45:29 +0100

    javascript tutorial

---
+++ app.js
@@ -0,0 +1,27 @@
+//let a = //
+//console.log(a)
+//console.log (typeof(a));
+// let firstName = 'John';
+// single quotes
+let lastName = "Doe";
+// double quotes
+let surname = `Ude`;
+// backticks
+
+// boolean
+// let isAdmin = true;
+// let isEmpolyed = false;
+
+//null 
+// let myName = null;
+
+// console.log  (typeof(isAdmin))
+// let num1 = 5
+// let num2= 3
+// console.log (num1 * num2);
+// console.log(2**6);
+// const fulName = lastName + " " + surname
+// let firstName = "agbaDev";
+// let stack = "node.js";
+// const introduction = `Hi ${firstName} Your stack is ${stack}`;
+// console.log(introduction)
